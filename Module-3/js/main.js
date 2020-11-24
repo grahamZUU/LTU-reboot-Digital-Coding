89760d4d5478419df2432ba997a6aae5720d875d
graham spencer <grahan58@gmail.com>
2020-11-24 12:22:43 +0000

    inital javascript

---
+++ Module-3/js/main.js
@@ -0,0 +1,9 @@
+alert("Hello Campers");
+console.log('secret message');
+document.write("Doc Write ...");
+var myHat = "Diving Hat";
+let myCat = "Tibbles";
+const pi = 3.142;
+document.write(myHat);
+document.write(myCat);
+document.write(pi);
---
+++ Module-3/js/task-3-1-2.js
@@ -0,0 +1,17 @@
+var theBill = 34.56;
+var serviceCharge = 1.1;
+var fullCost;
+var toPay;
+
+var first = '   Greg';
+var last = 'McDoogle   ';
+var full = first  +  ' .. ' + last;
+fullCost = theBill * serviceCharge;
+
+
+document.write(full);
+document.write('            Cost  =>  ' + fullCost);
+
+toPay = fullcost.tofixed();
+alert('    Cost = ' + toPay);
+
